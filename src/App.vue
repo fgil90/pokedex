623815c7df10b4f5121004814dd64a00998bb892
Alisson Lindote Novais <lindoten@gmail.com>
2022-09-04 20:30:22 -0300

    Add Header

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,38 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header id="header">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#" style="font-family: 'Pokemon Hollow'; font-size: 25px;">Pokemon</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#">Página Inicial <span class="sr-only">(current)</span></a>
+                    </li>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Emuladores
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Game Boy Colors</a>
+                        <a class="dropdown-item" href="#">Game Boy Advanced</a>
+                        <a class="dropdown-item" href="#">Nintendo DS</a>
+                        <a class="dropdown-item" href="#">Game Cube</a>
+                    </div>
+                  </li>
+                  <form class="form-inline">
+                      <div class="input-group">
+                      <div class="input-group-prepend">
+                          <span class="input-group-text ml-3" id="basic-addon1">Pokemon</span>
+                      </div>
+                      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+                      </div>
+                  </form>
+            </ul>
+          </div>
+        </nav>
   </header>
 
   <RouterView />
